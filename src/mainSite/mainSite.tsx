eda6901875fd96e4f3e6afef907175b94b90ba60
filmarzec <86653855+filmarzec@users.noreply.github.com>
2022-03-04 16:07:24 +0100

    changed layout

--- src/mainSite/mainSite.tsx
+++ src/mainSite/mainSite.tsx
@@ -1,3 +1,4 @@
+import { setSelectionRange } from '@testing-library/user-event/dist/utils';
 import React from 'react';
 import css from './mainSite.module.scss';
 
@@ -8,12 +9,25 @@ function mainSite(){
             <img src = "logo.png" className = {css.logo}></img>
             </div>
             <div className = {css.wrapper}>
-              
+            <div className = {css.left}>
+            <ul className = {css.list}>
+            <li className = {css.listElement}>T-SHIRTS</li>
+            <li className = {css.listElement}>HOODIES</li>
+            <li className = {css.listElement}>SWEATSHIRTS</li>
+            <li className = {css.listElement}>HATS</li>
+            <li className = {css.listElement}>BAGS</li>
+            <li className = {css.listElement}>ACCESORIES</li>
+            <li className = {css.listElement}>HARDWARE</li>
+            </ul>
+            </div>
             
             
             <div className = {css.middle}>
             
             </div>
+            <div className = {css.decoration}>
+            <img src = "snake.png" className = {css.right} ></img>
+              </div>
           </div>
         </div>
       );
